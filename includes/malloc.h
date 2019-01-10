113353a0bb57847216ff6c742410fe9c8262cbee
fedefloris <fzfloris@gmail.com>
2019-01-10 18:16:50 +0100

    Add includes folders and malloc.h

---
+++ includes/malloc.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffloris <ffloris@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/03 13:39:16 by ffloris           #+#    #+#             */
+/*   Updated: 2018/11/14 17:56:44 by ffloris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MALLOC_H
+# define MALLOC_H
+
+# include "libft.h"
+
+void		free(void *ptr);
+void		*malloc(size_t size);
+void		*realloc(void *ptr, size_t size);
+
+void		show_alloc_mem();
+#endif
