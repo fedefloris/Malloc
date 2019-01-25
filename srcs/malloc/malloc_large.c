b8f2ef2109e3dd2bcbf118893effa435979a1838
fedefloris <fzfloris@gmail.com>
2019-01-25 21:18:27 +0100

    Implement malloc for large zones

--- srcs/malloc/malloc_large.c
+++ srcs/malloc/malloc_large.c
@@ -12,17 +12,31 @@
 
 #include "malloc.h"
 
-void		*malloc_large(size_t size)
+static void		config_mem(char *mem, size_t size)
 {
-	void	*mem;
+	t_large_block	*block;
+	t_zone			*zone;
 
-	mem = mmap(0,
-		round_up_to_page_size(size),
+	zone = (t_zone*)mem;
+	zone->next = g_zones.larges;
+	g_zones.larges = zone;
+	block = (t_large_block*)(zone + 1);
+	block->size = size;
+}
+
+void			*malloc_large(size_t size)
+{
+	size_t	mem_size;
+	char	*mem;
+
+	mem_size = round_up_to_page_size(sizeof(t_zone) +
+		sizeof(t_large_block) + size);
+	mem = mmap(0, mem_size,
 		PROT_READ | PROT_WRITE,
 		MAP_ANONYMOUS | MAP_PRIVATE,
 		-1, 0);
 	if (mem == MAP_FAILED)
 		return (NULL);
-	// add block inside large zones
-	return (mem);
+	config_mem(mem, mem_size);
+	return (mem + sizeof(t_zone) + sizeof(t_large_block));
 }
