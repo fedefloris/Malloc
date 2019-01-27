49be0454c8f5b646b913c24d600076335ac421d3
fedefloris <fzfloris@gmail.com>
2019-01-27 23:08:56 +0100

    Implement find_block.c Closes #5

--- srcs/blocks/find_block.c
+++ srcs/blocks/find_block.c
@@ -12,8 +12,34 @@
 
 #include "malloc.h"
 
-t_block		*find_block(void *ptr)
+static t_zone	*find_zone(t_zone *zones, t_block *block)
 {
-	(void)ptr;
+	while (zones)
+	{
+		if ((char*)block >= (char*)(zones + 1)
+			&& (char*)block < (char*)zones + zones->size)
+			return (zones);
+		zones = zones->next;
+	}
+	return (NULL);
+}
+
+t_block			*find_block(void *ptr)
+{
+	t_block		*block;
+	t_block		*curr;
+	t_zone		*zone;
+
+	block = (t_block*)((char*)ptr - sizeof(t_block));
+	if (!(zone = find_zone(g_zones.tinies, block))
+		&& !(zone = find_zone(g_zones.smalls, block)))
+		return (NULL);
+	curr = (t_block*)(zone + 1);
+	while ((char*)curr < (char*)zone + zone->size)
+	{
+		if (curr == block)
+			return (block);
+		curr = (t_block*)((char*)curr + (1 << curr->size));
+	} // should I really check all blocks?
 	return (NULL);
 }
