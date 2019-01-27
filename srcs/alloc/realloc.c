19663ad1e79d6d7ae92f05c0e312e3cb59736783
fedefloris <fzfloris@gmail.com>
2019-01-27 23:18:32 +0100

    Add some pseudocode in realloc.c

--- srcs/alloc/realloc.c
+++ srcs/alloc/realloc.c
@@ -14,9 +14,16 @@
 
 void		*realloc(void *ptr, size_t size)
 {
-	(void)ptr;
-	(void)size;
-	// what do we do with size of the block size?
-	// Check standard behaviour of realloc
+	if (!ptr)
+		return (malloc(size));
+	if (!size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	// check valid ptr, use same functions of free
+	// try to change size
+	// if it's not possible
+	//   call malloc with the new size, memcpy and free previous
 	return (NULL);
 }
