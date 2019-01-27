9500325b46762e81582307feb8615d326301690a
fedefloris <fzfloris@gmail.com>
2019-01-27 12:47:55 +0100

    Get power of 2 given requested size

--- srcs/alloc/malloc_small.c
+++ srcs/alloc/malloc_small.c
@@ -19,19 +19,34 @@ static t_zone	*get_zones(int zone_size)
 	return (g_zones.smalls);
 }
 
+static int		get_power_of_2(size_t request)
+{
+	int			power_of_2;
+	size_t		size;
+
+	size = 16;
+	power_of_2 = 4;
+	while (size < request)
+	{
+		power_of_2++;
+		size *= 2;
+	}
+	return (power_of_2);
+}
+
 void			*malloc_small(size_t size, int zone_size)
 {
+	int			power_of_2;
 	t_zone		*zones;
 	t_block		*block;
 
 	block = NULL;
 	zones = get_zones(zone_size);
-	(void)zones;
-	(void)size;
-	// get power of 2 that fits requested size
-	// if no zones get one
-	// for every zone
-	//  search a free block
-	// return ((void*)(block + 1));
-	return (block);
+	power_of_2 = get_power_of_2(sizeof(t_block) + size);
+	ft_printf("power of 2: %zu\n", power_of_2);
+	if (!(block = get_block_from_zones(zones, power_of_2)))
+		{}// add_new_zone() get_block_from_zones()
+	if (!block)
+		return (NULL);
+	return ((void*)(block + 1));
 }
