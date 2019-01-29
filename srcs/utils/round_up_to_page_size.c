f3b9629811a9893da092708d8a46307d64fc7a3e
ffloris <fzfloris@gmail.com>
2019-01-29 20:57:34 +0100

    Norminette for utils folder (except C++ comments)

--- srcs/utils/round_up_to_page_size.c
+++ srcs/utils/round_up_to_page_size.c
@@ -6,13 +6,13 @@
 /*   By: ffloris <ffloris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/03 13:39:16 by ffloris           #+#    #+#             */
-/*   Updated: 2018/11/14 17:56:44 by ffloris          ###   ########.fr       */
+/*   Updated: 2019/01/29 20:55:31 by ffloris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-size_t      round_up_to_page_size(size_t size)
+size_t		round_up_to_page_size(size_t size)
 {
 	long	page_size;
 
