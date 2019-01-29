67678081a1b4a518698ccfd0df4593feeca93f67
ffloris <fzfloris@gmail.com>
2019-01-29 20:57:08 +0100

    Norminette for display folder (except C++ comments)

--- srcs/display/show_alloc_mem.c
+++ srcs/display/show_alloc_mem.c
@@ -6,13 +6,13 @@
 /*   By: ffloris <ffloris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/03 13:39:16 by ffloris           #+#    #+#             */
-/*   Updated: 2018/11/14 17:56:44 by ffloris          ###   ########.fr       */
+/*   Updated: 2019/01/29 20:54:50 by ffloris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-void			show_alloc_mem()
+void		show_alloc_mem(void)
 {
 	ft_printf("\n\n");
 	display_zones(g_zones.tinies, "TINY");
