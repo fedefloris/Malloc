c69d264d23acba4ed0529599f719382b31ab4f3a
fedefloris <fzfloris@gmail.com>
2019-02-28 15:33:11 +0100

    Implement free without defragmentation. Closes #14

--- srcs/free/free_small_block.c
+++ srcs/free/free_small_block.c
@@ -18,25 +18,27 @@ void			free_small_block(t_block **blocks, t_block *block,
 	t_zone *zone, int zone_type)
 {
 	size_t		size_log2;
-	size_t		max_log2;
+	// size_t		max_log2;
 	// t_block		*buddy;
 
-	(void)zone;
-	(void)blocks;
 	if (!block)
 		return ; // invalid pointer for block
-	if (zone_type == TINY_ZONE_SIZE)
-		max_log2 = TINY_MAX_LOG2;
-	else
-		max_log2 = SMALL_MAX_LOG2;
+	(void)zone_type;
+	// if (zone_type == TINY_ZONE_SIZE)
+	// 	max_log2 = TINY_MAX_LOG2;
+	// else
+	// 	max_log2 = SMALL_MAX_LOG2;
 	size_log2 = block->size_log2;
-	while (size_log2 <= max_log2)
-	{
+	(void)zone;
+	block->next = blocks[size_log2];
+	blocks[size_log2] = block;
+	// while (size_log2 <= max_log2)
+	// {
 		// buddy = (t_block*)BUDDY((char*)(blocks + max_log2 + 1),
 		// 	(char*)block, i - 1);
 		// search BUDDY in free list of block->size_log2
 		// if BUDDY found merge blocks
-		size_log2++;
-	}
+	// 	size_log2++;
+	// }
 	// when should I call munmap?
 }
