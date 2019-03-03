1e23eb539ef37b2c75bba65689faa549b8759bbe
ffloris <fzfloris@gmail.com>
2019-03-03 12:07:28 +0100

    Add test_malloc in tests/malloc

--- tests/malloc/test_malloc.c
+++ tests/malloc/test_malloc.c
@@ -22,14 +22,7 @@ int				main(void)
 			break ;
 		}
 		test[i]->a[2] = 'a';
-		// ft_printf("Before free()\n");
-		// show_alloc_mem();
-		free(test[i]);
-		ft_printf("After free()\n");
-		show_alloc_mem();
 		i++;
 	}
-	ft_printf("End...\n");
-	show_alloc_mem();
 	return (0);
 }
