66a6cd60500609f3b136988c7bd03a02efd002c6
Federico Floris <fzfloris@gmail.com>
2019-03-07 16:58:10 +0100

    Start to implement test.sh #11

--- tests/test.sh
+++ tests/test.sh
@@ -1,1 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
+set env DYLD_INSERT_LIBRARIES ./libft_malloc.so
+
+echo "Testing ls"
+LD_PRELOAD=./libft_malloc.so ls
+LD_PRELOAD=./libft_malloc.so ls -Rlh
+LD_PRELOAD=./libft_malloc.so ls /
+
+echo "Testing find"
+LD_PRELOAD=./libft_malloc.so find .
