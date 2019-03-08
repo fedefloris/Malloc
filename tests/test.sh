78290ed5aefdf7312af031a3e2dee928e7829470
Federico Floris <fzfloris@gmail.com>
2019-03-08 14:59:21 +0100

    Using tput command in test.sh #11

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,24 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set env DYLD_INSERT_LIBRARIES ./libft_malloc.so
 
-echo "Testing ls"
-LD_PRELOAD=./libft_malloc.so ls
-LD_PRELOAD=./libft_malloc.so ls -Rlh
-LD_PRELOAD=./libft_malloc.so ls /
+# Colors
+RED=$(tput setaf 1)
+WHITE=$(tput setaf 7)
+GREEN=$(tput setaf 2)
 
-echo "Testing find"
-LD_PRELOAD=./libft_malloc.so find .
+#Text styles
+BOLD_TEXT=$(tput bold)
+NORMAL_TEXT=$(tput sgr0)
+
+# Testing 16-byte alignment
+commands=(
+    "ls"
+    "ls -lRh"
+    "ls -l /"
+    "find ."
+)
+for command in "${commands[@]}"; do
+    LD_PRELOAD=./libft_malloc.so ${command} 2>&1 > /dev/null
+    echo "Testing with ${BOLD_TEXT}${command}${NORMAL_TEXT} ${GREEN}OK${WHITE}"
+done
