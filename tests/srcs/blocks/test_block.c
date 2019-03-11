826b1e418d2c801c537014a8b5f61a583c324c2c
ffloris <fzfloris@gmail.com>
2019-03-11 11:12:06 +0100

    Test 16-bytes alignment in test_block.c #11

--- tests/srcs/blocks/test_block.c
+++ tests/srcs/blocks/test_block.c
@@ -18,6 +18,8 @@ void			test_block(void *ptr, size_t size, t_block_status status)
 	t_zone		*zone;
 	t_block		*block;
 
+	if ((intptr_t)ptr % 16 != 0)
+		error_exit("ptr is not 16-byte aligned");
 	block = get_block_info(ptr, &zone, &zone_type);
 	if (zone_type == LARGE_THRESHOLD)
 		return ;
